74a6d52430654497f706f71d3cc76c8ce488ea49
AndAgio <aagiollo@gmail.com>
2023-03-23 19:30:13 +0100

    Feat: setup for jetson nano.

--- exes/nano_setup.sh
+++ exes/nano_setup.sh
@@ -5,19 +5,20 @@ sudo apt install -y python3.8 python3.8-venv python3.8-dev
 sudo apt install -y libopenmpi-dev libomp-dev libopenblas-dev libblas-dev libeigen3-dev libcublas-dev
 
 cd ..
-git clone https://github.com/ultralytics/ultralytics
-cd ultralytics
-
 python3.8 -m venv timelapse_env
 source timelapse_env/bin/activate
+
+git clone https://github.com/ultralytics/ultralytics
+cd ultralytics
 
 pip install -U pip wheel gdown
 
 # pytorch 1.11.0
-gdown https://drive.google.com/uc?id=1hs9HM0XJ2LPFghcn7ZMOs5qu5HexPXwM
+gdown https://drive.google.com/uc?id=1hs9HM0XJ2LPFghcn7ZMOs5qu5HexPXwM torch-1.11.010+gitbc2c6ed-cp38-cp38-linux_aarch64.whl
 # torchvision 0.12.0
-gdown https://drive.google.com/uc?id=1m0d8ruUY8RvCP9eVjZw4Nc8LAwM8yuGV
-python3.8 -m pip install torch-*.whl torchvision-*.whl
+gdown https://drive.google.com/uc?id=1m0d8ruUY8RvCP9eVjZw4Nc8LAwM8yuGV torchvision-0.12.0a0+9b5a3fe-cp38-cp38-linux_aarch64.whl
+python3.8 -m pip install torch-1.11.010+gitbc2c6ed-cp38-cp38-linux_aarch64.whl
+python3.8 -m pip install torchvision-0.12.0a0+9b5a3fe-cp38-cp38-linux_aarch64.whl
 
 python3.8 -m pip install .
 
