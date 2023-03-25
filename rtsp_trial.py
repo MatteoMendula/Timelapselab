dcb3b947fa4619173cb675d85679a286826e7b2c
AndAgio <aagiollo@gmail.com>
2023-03-25 21:09:47 +0100

    Feat: resize image in rtsp retrieval.

--- rtsp_trial.py
+++ rtsp_trial.py
@@ -1,6 +1,15 @@
 import rtsp
 
-RTSP_URL = 'rtsp://admin:Mp010201@10.111.45.211:554/Streaming/channels/101'
 
-with rtsp.Client(RTSP_URL) as client:
-    client.preview()
+def process_image(image):
+    return image.resize([640, 640])
+
+
+if __name__ == '__main__':
+
+    rstp_url = 'rtsp://admin:Mp010201@10.111.45.211:554/Streaming/channels/101'
+    with rtsp.Client(rstp_url) as client:
+
+        while True:
+            process_image(client.read(raw=True)).show()
+
